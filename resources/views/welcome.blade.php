89c495bfcb9e3fd6c66832f97da0875e1ce54974
irvin_bedolla <irvinsbm@gmail.com>
2025-01-20 15:35:07 -0600

    Actualizacion de home, cambio de iconos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 
         <!-- Bootstrap core CSS -->
         <link href="public/assets_seer/assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
+        <link rel="icon" href="public/assets_seer/images/icono.png" type="image/x-icon">
         <style>
             .bd-placeholder-img {
                 font-size: 1.125rem;
@@ -38,6 +38,7 @@
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 3"></button>
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="4" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="5" aria-label="Slide 5"></button>
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
@@ -54,25 +55,6 @@
                                 <p>
                                     <a class="btn btn-lg btn-primary" href="https://michoacan.cencolab.mx/asesoria/seleccion" style="text-decoration: none; background-color: #CEA845;">
                                         Generar Solicitud
-                                    </a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <rect width="100%" height="100%" fill="#6A0F49"/>
-                        </svg>
-                        <div class="container">
-                            <div class="carousel-caption text-end">
-                                <img class="Absbottom" src="public/assets_seer/images/ccl-r.png" alt="" style="max-width: 50%; position:relative; top:100px; right:70px;">
-                            </div>
-                            <div class="carousel-caption text-start">
-                                <h1>Registro de <br>representaciones legales.</h1>
-                                <p>Inicia tu registro como apoderado.</p>
-                                <p>
-                                    <a class="btn btn-lg btn-primary" href="{{ route('poder-crear') }}" style="text-decoration: none; background-color: #CEA845;">
-                                        Generar Registro
                                     </a>
                                 </p>
                             </div>
@@ -111,6 +93,44 @@
                                 <p>
                                     <a class="btn btn-lg btn-primary" href="https://cclmichoacan.gob.mx/Calculadora.html" style="text-decoration: none; background-color: #CEA845;">
                                         Realizar cálculo.
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <rect width="100%" height="100%" fill="#6A0F49"/>
+                        </svg>
+                        <div class="container">
+                            <div class="carousel-caption text-end">
+                                <img class="Absbottom" src="public/assets_seer/images/ccl-r.png" alt="" style="max-width: 50%; position:relative; top:100px; right:70px;">
+                            </div>
+                            <div class="carousel-caption text-start">
+                                <h1>Registro de <br>representaciones legales.</h1>
+                                <p>Inicia tu registro como apoderado.</p>
+                                <p>
+                                    <a class="btn btn-lg btn-primary" href="{{ route('poder-crear') }}" style="text-decoration: none; background-color: #CEA845;">
+                                        Generar Registro
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <rect width="100%" height="100%" fill="#6A0F49"/>
+                        </svg>
+                        <div class="container">
+                            <div class="carousel-caption text-end">
+                                <img class="Absbottom" src="public/assets_seer/images/ccl-r.png" alt="" style="max-width: 50%; position:relative; top:100px; right:70px;">
+                            </div>
+                            <div class="carousel-caption text-start">
+                                <h1>Cursos y Capacitaciones.</h1>
+                                <p>Ingresar a los cursos.</p>
+                                <p>
+                                    <a class="btn btn-lg btn-primary" href="{{ route('poder-crear') }}" style="text-decoration: none; background-color: #CEA845;">
+                                        Inicio
                                     </a>
                                 </p>
                             </div>
@@ -185,7 +205,7 @@
                     <div class="col-lg-4">
                         <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
                         <h2>Lázaro Cardenas</h2>
-                        <p>Calle paracho número 26 colonia 600 casas C.p. 60950</p>
+                        <p>CALLE PARACHO NUMERO 26 COLONIA 600 CASAS CP. 60950</p>
                         <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Paracho+26,+600+Casas,+60954+Cdad.+L%C3%A1zaro+C%C3%A1rdenas,+Mich./@17.9664748,-102.2234345,17z/data=!3m1!4b1!4m10!1m2!2m1!1sCalle+paracho+n%C3%BAmero+26+colonia+600+casas+C.p.+60950!3m6!1s0x84315c8d3b8b9bdf:0xe98a2dd18850e4cf!8m2!3d17.9664698!4d-102.2185636!15sCjVDYWxsZSBwYXJhY2hvIG7Dum1lcm8gMjYgY29sb25pYSA2MDAgY2FzYXMgQy5wLiA2MDk1MJIBEWNvbXBvdW5kX2J1aWxkaW5n4AEA!16s%2Fg%2F11fsjvfnh9?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D">Ver Ubicación &raquo;</a></p>
                     </div><!-- /.col-lg-4 -->
                 </div><!-- /.row -->
