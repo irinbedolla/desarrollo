04a3ce52a758f5fe9fab59c6319f094815b27e9e
ofeliaTR <otapiarodriguez@gmail.com>
2025-03-13 15:45:20 -0600

    modificaciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,22 +21,25 @@
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <img src="public/assets_seer/images/Logos gobiernos.png" class="img" style="width: 10%"></a>&nbsp;&nbsp;
-        <img src="public/assets_seer/images/logo-ccl.png" class="img" style="width: 10%"></a>
+        <img src="public/assets_seer/images/Logos gobiernos.png" class="img" style="max-width: 8%"></a>&nbsp;&nbsp;
+        <img src="public/assets_seer/images/logo-ccl.png" class="img" style="max-width: 4%"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link"  style="color: black;">INICIO<span class="sr-only"></span></a>
+                    <a class="nav-link" href="#" style="color: black;">INICIO<span class="sr-only"></span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: black;">SERVICIOS</a>
+                    <a class="nav-link" href="#servicios" style="color: black;">SERVICIOS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href=""style="color: black;">SEDES</a>
+                    <a class="nav-link" href="#sedes"style="color: black;">SEDES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contacto"style="color: black;">CONTACTO</a>
                 </li>
             </ul>
         </div>
@@ -44,86 +47,124 @@
 
 
     <main>
-
+    <section id="servicios">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-
-
-    <div class="card-group">
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-    </main>
-
-
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="public/assets_seer/images/1.jpeg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="public/assets_seer/images/4.jpeg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="public/assets_seer/images/8.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
       </a>
-      <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
+  </section>
+  <div class="card-group">
+    <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+  </div>
+  <section id="sedes">
+        <br><br><br><br><br><br>
+        <h3 class="wow fadeInLeft" style="text-align: center ; font-family:Nutmeg, Sans-serif;">Nuestras sedes</h3> <br><br><br>
+        <div class="container marketing">
+            <!-- Three columns of text below the carousel -->
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-2">
+                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
+                    <h2>Morelia</h2>
+                    <p>BLVD. GARCÍA DE LEÓN 1575, CHAPULTEPEC ORIENTE, C.P. 58260 MORELIA, MICH.</p>
+                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Centro+de+Conciliaci%C3%B3n+Laboral+del+Estado+de+Michoac%C3%A1n/@19.6886808,-101.1665464,17z/data=!3m1!4b1!4m6!3m5!1s0x86972e4da3b81177:0xb3bdb18efbe90610!8m2!3d19.6886758!4d-101.1639715!16s%2Fg%2F11tjwgnyhz?entry=ttu" target="_blank">Ver Ubicación &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-2">
+                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
+                    <h2>Uruapan</h2>
+                    <p>NUEVO PARICUTÍN NO 308, COL. JARDINES DE SAN RAFAEL, C.P. 30136 URUAPAN MICHOACÁN.</p>
+                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Centro+de+Conciliaci%C3%B3n+Laboral+Uruapan/@19.4183803,-102.0080912,17z/data=!3m1!4b1!4m6!3m5!1s0x842de3549b0e069b:0x5044e26ce5f7e25a!8m2!3d19.4183753!4d-102.0055163!16s%2Fg%2F11ryt409m2?entry=ttu" target="_blank">Ver Ubicación &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-2">
+                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
+                    <h2>Zamora</h2>
+                    <p>JUSTO SIERRA PONIENTE NO 290, CP. COL. JARDINES DE CATEDRAL, C.P. 59600 ZAMORA, MICHOACÁN.</p>
+                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Justo+Sierra+209,+Jardines+de+Catedral,+Zamora+de+Hidalgo,+Mich./@19.9820742,-102.2817188,17z/data=!3m1!4b1!4m5!3m4!1s0x842e88c79f23a3c5:0x70d579455d9255cb!8m2!3d19.9820692!4d-102.2791439?entry=ttu">Ver Ubicación &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-2">
+                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
+                    <h2>Zitácuaro</h2>
+                    <p>CALLE CUAUHTEMOC ORIENTE 15, ZITÁCUARO, MICHOACÁN.</p>
+                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Cuauht%C3%A9moc+Ote.+15,+Cuauhtemoc,+61506+Zit%C3%A1cuaro,+Mich./@19.4385694,-100.3564309,17z/data=!3m1!4b1!4m6!3m5!1s0x85d2a4a1dec711b3:0x68685762f8695d11!8m2!3d19.4385644!4d-100.353856!16s%2Fg%2F11c21h6jl_?entry=ttu">Ver Ubicación &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-2">
+                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
+                    <h2>Lázaro Cardenas</h2>
+                    <p>CALLE PARACHO NUMERO 26 COLONIA 600 CASAS CP. 60950</p>
+                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Paracho+26,+600+Casas,+60954+Cdad.+L%C3%A1zaro+C%C3%A1rdenas,+Mich./@17.9664748,-102.2234345,17z/data=!3m1!4b1!4m10!1m2!2m1!1sCalle+paracho+n%C3%BAmero+26+colonia+600+casas+C.p.+60950!3m6!1s0x84315c8d3b8b9bdf:0xe98a2dd18850e4cf!8m2!3d17.9664698!4d-102.2185636!15sCjVDYWxsZSBwYXJhY2hvIG7Dum1lcm8gMjYgY29sb25pYSA2MDAgY2FzYXMgQy5wLiA2MDk1MJIBEWNvbXBvdW5kX2J1aWxkaW5n4AEA!16s%2Fg%2F11fsjvfnh9?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D">Ver Ubicación &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+                <!-- START THE FEATURETTES -->
+                <!--<hr class="featurette-divider">-->
+        </div><!-- /.container -->
+    </section>
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-    </ul>
-  </footer>
-    <!--<script src="public/assets_seer/assets/dist/js/bootstrap.bundle.min.js"></script>-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </main>
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+          <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+      </div>
+
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+      </ul>
+    </footer>
+      <!--<script src="public/assets_seer/assets/dist/js/bootstrap.bundle.min.js"></script>-->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
