e51a7065fc505bd27091a04a3d505b6361d53329
irvin bedolla <irvinsbm@gmail.com>
2025-03-13 13:59:22 -0600

    Base de vista welcome responsiva

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,214 +9,120 @@
         <link href="public/assets/css/carousel.css" rel="stylesheet">
         <title>Si Conciliación</title>
         <!-- Bootstrap core CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <link rel="icon" href="public/assets_seer/images/logo-ccl.png" type="image/x-icon">
         <style>
-            .bd-placeholder-img {
-                font-size: 1.125rem;
-                text-anchor: middle;
-                -webkit-user-select: none;
-                -moz-user-select: none;
-                user-select: none;
-            }
-
-            @media (min-width: 768px) {
-                .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-                }
-            }
-              
+            
         </style>
         <!-- Custom styles for this template -->
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="rd-navbar-panel">
-            <div class="rd-navbar-brand">
-                <img src="public/assets_seer/images/logo-ccl.png"class="img-fluid, mx-auto, my-auto" style="max-width: 4%; position:relative;"></a>
-                <img src="public/assets_seer/images/Logos gobiernos.png"class="img-fluid, mx-auto, my-auto" style="max-width: 8%; position:relative;"></a>
-            </div>
-		</div>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav ms-auto">
+        <img src="public/assets_seer/images/Logos gobiernos.png" class="img" style="width: 10%"></a>&nbsp;&nbsp;
+        <img src="public/assets_seer/images/logo-ccl.png" class="img" style="width: 10%"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#" style="color: black;">INICIO<span class="sr-only"></span></a>
+                    <a class="nav-link"  style="color: black;">INICIO<span class="sr-only"></span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#servicios" style="color: black;">SERVICIOS</a>
+                    <a class="nav-link" href="#" style="color: black;">SERVICIOS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#sedes"style="color: black;">SEDES</a>
+                    <a class="nav-link" href=""style="color: black;">SEDES</a>
                 </li>
             </ul>
-            <span class="navbar-text"></span>
         </div>
     </nav>
+
+
     <main>
-    <section id="servicios">
-	    <h3 class="wow fadeInLeft" style="text-align: center ; font-family:Nutmeg, Sans-serif;">Nuestros Servicios</h3> <br><br><br>
-        <div style="display: block; text-align: center;">
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <h2 style="font-size: 20px; font-family: Arial; color: #000000">Solicitud en línea</h2> <img
-                        src="public/assets_seer/images/contrato.png" alt="Descripción de la imagen"
-                        style="width: 100px; height: auto;">
-                    </div>
-                    <div class="flip-box-back">
-                        <a href="{{ route('solicitud') }}" target="_blank" style="display: contents;">
-                            <h2 style="font-size: 22px; font-family: Arial; color:#ffffff">Es un servicio rápido, eficiente que permite a las personas, tanto trabajadoras como empleadoras iniciar su solicitud para conciliar de forma digital a través de la página web.</h2>
-                        </a>
-                    </div>
-                </div>
-            </div>    
-        </div>
-        <div class="flip-box">
-            <div class="flip-box-inner">
-                <div class="flip-box-front">
-                    <h2 style="font-size: 22px; font-family: Arial; color:#000000">Citas para ratificación</h2> <img
-                        src="public/assets_seer/images/calendario-diario.png" alt="Descripción de la imagen"
-                        style="width: 100px; height: auto;">
-                </div>
-                <div class="flip-box-back">
-                    <a href="{{ route('create_cita') }}" target="_self" style="display: contents;">
-                        <h2 style="font-size: 22px; font-family: Arial; color:#ffff">Es un servicio que permite a las partes que terminan su relación laboral, acudir con previa cita ante el Centro de Concicliación Laboral a ratificar su acuerdo, con el fin de brindar seguridad jurídica.</h2>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="flip-box">
-            <div class="flip-box-inner">
-                <div class="flip-box-front">
-                    <h2 style="font-size: 22px; font-family: Arial; color:#000000">Calculadora de <br>prestaciones</h2>
-                    <img src="public/assets_seer/images/calculadora.png" alt="Descripción de la imagen"
-                        style="width: 100px; height: auto;">
-                </div>
-                <div class="flip-box-back">
-                    <a href="https://cclmichoacan.gob.mx/Calculadora.html" target="_blank"
-                        style="display: contents; color: white;">
-                        <h2 style="font-size: 21px; font-family: Arial; color:#ffff">Es una herramienta digital que permite a las personas, conocer los cálculos aproximados de las prestaciones laborales, que serán consideradas dentro de la audiencia de conciliación.</h2>
-                    </a>
-                </div>
-            </div>    
-        </div>
-        <div class="flip-box">
-            <div class="flip-box-inner">
-                <div class="flip-box-front">
-                    <h2 style="font-size: 22px; font-family: Arial; color:#000000">Registro de <br>representaciones legales</h2> <img
-                        src="public/assets_seer/images/registro.png" alt="Descripción de la imagen"
-                        style="width: 100px; height: auto;">
-                </div>
-                <div class="flip-box-back">
-                    <a href="{{ route('poder-crear') }}" target="_self"
-                        style="display: contents; color: white;">
-                        <h2 style="font-size: 21px; font-family: Arial; color:#ffff"><br>Es una plataforma digital, que permite a las personas empleadoras registrar a sus representantes legales, con la finalidad de agilizar el procedimiento de conciliación.</h2>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="flip-box">
-            <div class="flip-box-inner">
-                <div class="flip-box-front">
-                    <h2 style="font-size: 22px; font-family: Arial; color:#000000">SEER</h2>
-                    <img src="public/assets_seer/images/ccl-r.png" alt="Descripción de la imagen"
-                        style="width: 250px; height: auto;">
-                </div>
-                <div class="flip-box-back">
-                    <a href="{{ route('login') }}" target="_self"
-                        style="display: contents; color: white;">
-                        <h2 style="font-size: 21px; font-family: Arial; color:#ffff"><br>Sistema estadistica y evaluación de resultados.</h2>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="flip-box">
-            <div class="flip-box-inner">
-                <div class="flip-box-front">
-                    <h2 style="font-size: 22px; font-family: Arial; color:#000000">Cursos y Capacitaciones</h2>
-                    <img src="public/assets_seer/images/leccion.png" alt="Descripción de la imagen"
-                        style="width: 100px; height: auto;">
-                </div>
-                <div class="flip-box-back">
-                    <a href="#" target="_self"
-                        style="display: contents; color: white;">
-                        <h2 style="font-size: 21px; font-family: Arial; color:#ffff"><br>Ingresar a los cursos.</h2>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="flip-box">
-            <div class="flip-box-inner">
-                <div class="flip-box-front">
-                    <h2 style="font-size: 22px; font-family: Arial; color:#000000">Asesoría virtual</h2>
-                    <img src="public/assets_seer/images/chatbot.png" alt="Descripción de la imagen"
-                        style="width: 100px; height: auto;">
-                </div>
-                <div class="flip-box-back">
-                    <a href="{{ route('chat') }}" onclick="window.open(this.href, 'mywin', 'left=' + (window.innerWidth - 500) + ',top=' + (window.innerHeight - 550) + ',width=500,height=550,toolbar=1,resizable=0'); return false;" target="_self" style="display: contents; color: white;">
-                        <h2 style="font-size: 21px; font-family: Arial; color:#ffff"><br>Es un servicio informático que podrás consultar con la finalidad de atender y resolver preguntas acerca de nuestros servicios.</h2>
-                    </a>
-                </div>
-            </div>
-	    </div>
-    </section>
-    <section id="sedes">
-        <br><br><br><br><br><br>
-        <h3 class="wow fadeInLeft" style="text-align: center ; font-family:Nutmeg, Sans-serif;">Nuestras sedes</h3> <br><br><br>
-        <div class="container marketing">
-            <!-- Three columns of text below the carousel -->
-            <div class="row d-flex justify-content-center">
-                <div class="col-lg-2">
-                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
-                    <h2>Morelia</h2>
-                    <p>BLVD. GARCÍA DE LEÓN 1575, CHAPULTEPEC ORIENTE, C.P. 58260 MORELIA, MICH.</p>
-                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Centro+de+Conciliaci%C3%B3n+Laboral+del+Estado+de+Michoac%C3%A1n/@19.6886808,-101.1665464,17z/data=!3m1!4b1!4m6!3m5!1s0x86972e4da3b81177:0xb3bdb18efbe90610!8m2!3d19.6886758!4d-101.1639715!16s%2Fg%2F11tjwgnyhz?entry=ttu" target="_blank">Ver Ubicación &raquo;</a></p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-lg-2">
-                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
-                    <h2>Uruapan</h2>
-                    <p>NUEVO PARICUTÍN NO 308, COL. JARDINES DE SAN RAFAEL, C.P. 30136 URUAPAN MICHOACÁN.</p>
-                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Centro+de+Conciliaci%C3%B3n+Laboral+Uruapan/@19.4183803,-102.0080912,17z/data=!3m1!4b1!4m6!3m5!1s0x842de3549b0e069b:0x5044e26ce5f7e25a!8m2!3d19.4183753!4d-102.0055163!16s%2Fg%2F11ryt409m2?entry=ttu" target="_blank">Ver Ubicación &raquo;</a></p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-lg-2">
-                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
-                    <h2>Zamora</h2>
-                    <p>JUSTO SIERRA PONIENTE NO 290, CP. COL. JARDINES DE CATEDRAL, C.P. 59600 ZAMORA, MICHOACÁN.</p>
-                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Justo+Sierra+209,+Jardines+de+Catedral,+Zamora+de+Hidalgo,+Mich./@19.9820742,-102.2817188,17z/data=!3m1!4b1!4m5!3m4!1s0x842e88c79f23a3c5:0x70d579455d9255cb!8m2!3d19.9820692!4d-102.2791439?entry=ttu">Ver Ubicación &raquo;</a></p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-lg-2">
-                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
-                    <h2>Zitácuaro</h2>
-                    <p>CALLE CUAUHTEMOC ORIENTE 15, ZITÁCUARO, MICHOACÁN.</p>
-                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Cuauht%C3%A9moc+Ote.+15,+Cuauhtemoc,+61506+Zit%C3%A1cuaro,+Mich./@19.4385694,-100.3564309,17z/data=!3m1!4b1!4m6!3m5!1s0x85d2a4a1dec711b3:0x68685762f8695d11!8m2!3d19.4385644!4d-100.353856!16s%2Fg%2F11c21h6jl_?entry=ttu">Ver Ubicación &raquo;</a></p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-lg-2">
-                    <img src="public/assets_seer/images/logo-ccl.png" alt="" style="max-width: 30%; height: auto;">
-                    <h2>Lázaro Cardenas</h2>
-                    <p>CALLE PARACHO NUMERO 26 COLONIA 600 CASAS CP. 60950</p>
-                    <p><a class="btn btn-secondary" href="https://www.google.com/maps/place/Paracho+26,+600+Casas,+60954+Cdad.+L%C3%A1zaro+C%C3%A1rdenas,+Mich./@17.9664748,-102.2234345,17z/data=!3m1!4b1!4m10!1m2!2m1!1sCalle+paracho+n%C3%BAmero+26+colonia+600+casas+C.p.+60950!3m6!1s0x84315c8d3b8b9bdf:0xe98a2dd18850e4cf!8m2!3d17.9664698!4d-102.2185636!15sCjVDYWxsZSBwYXJhY2hvIG7Dum1lcm8gMjYgY29sb25pYSA2MDAgY2FzYXMgQy5wLiA2MDk1MJIBEWNvbXBvdW5kX2J1aWxkaW5n4AEA!16s%2Fg%2F11fsjvfnh9?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D">Ver Ubicación &raquo;</a></p>
-                </div><!-- /.col-lg-4 -->
-            </div><!-- /.row -->
-                <!-- START THE FEATURETTES -->
-                <hr class="featurette-divider">
-        </div><!-- /.container -->
-    </section>
-        <!-- FOOTER -->
-        <footer class="container">
-        <div class="footer-logo">
-				<img src="public/assets_seer/images/logo-ccl.png" alt="Logo Michoacán">
-		    </div>
-            <div class="footer-socials">
-				<a href="https://www.facebook.com/share/p/5ZkRpPXkcLU5f22B/?mibextid=QwDbR1" class="social-icon" target="_blank"><img src="public/assets_seer/images/Face.png" ></a>
-				<a href="https://www.instagram.com/p/DAHE5ccyEuf/?igsh=MTZoOTFzdjJ6MzZ4cg==" class="social-icon" target="_blank"><img src="public/assets_seer/images//tw.png"  ></i></a>
-			    <a href="https://x.com/cclmichoacan/status/1836850990471074230?s=46&t=b2fwr9Vcf8iWYVavE8GJgA" target="_blank" class="social-icon"><img src="public/assets_seer/images/Twr.png"  ></i></a>
-			</div>
-			<div class="footer-hashtag">
-            <p class="float-end"><!--<a href="#">Regresar</a>--></p>
-            <p>&copy; 2023–2027.</p>
-        </footer>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+    <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
     </main>
+
+
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+    </ul>
+  </footer>
     <!--<script src="public/assets_seer/assets/dist/js/bootstrap.bundle.min.js"></script>-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
